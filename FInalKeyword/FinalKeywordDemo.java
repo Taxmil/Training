a7ac9a83c49bec0305069cdb8aa1d57f1b527094
Taxmil <146539293+Taxmil@users.noreply.github.com>
2025-02-12 09:31:23 +0530

    Delete FInalKeyword directory

--- FInalKeyword/FinalKeywordDemo.java
+++
@@ -1,9 +0,0 @@
-public class FinalKeywordDemo {
-    public static void main(String[] args) {
-        Final finalClass = new Final();
-        
-        System.out.println("Accessing final variable: " + finalClass.finalVariable);
-        
-        finalClass.finalMethod();
-    }
-}
