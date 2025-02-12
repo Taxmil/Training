c1dba751d209e172886568187f175f55da9dfe60
Taxmil <146539293+Taxmil@users.noreply.github.com>
2025-02-12 09:00:56 +0530

    Create FInal.java

---
+++ FInalKeyword/FInal.java
@@ -0,0 +1,11 @@
+final class Final {
+    final int finalVariable = 100;
+
+    public final void finalMethod() {
+        System.out.println("This is a final method in the FinalClass.");
+    }
+
+    public void display() {
+        System.out.println("The final variable value is: " + finalVariable);
+    }
+}
