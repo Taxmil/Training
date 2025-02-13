f95d3612f34b3c178ec598a9d5abc6bee339a88d
Taxmil <146539293+Taxmil@users.noreply.github.com>
2025-02-13 08:20:47 +0530

    Add files via upload

---
+++ twelth_feb/Employee.java
@@ -0,0 +1,15 @@
+package twelve;
+
+import java.io.Serializable;
+
+class Employee implements Serializable
+{
+    Employee(int eno, String name,String email){
+        this.eno=eno;
+        this.name=name;
+        this.email=email;
+    }
+    int eno;
+    String name;
+    transient String email;
+}
